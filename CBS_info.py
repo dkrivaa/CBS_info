1e40c4e01b368827421aeba8dcb3e99fe013390a
dkrivaa <dannykrivaa@hotmail.com>
2023-06-14 23:32:09 +0300

    App to see CBS data-series info

--- CBS_info.py
+++ CBS_info.py
@@ -105,3 +105,28 @@ if response.status_code == 200:
     choice4 = st.selectbox('Level 4 - Select area of interest', df4['name'])
     path4 = (df4['path'][df4['name'].tolist().index(choice4)])
     st.write(path4)
+
+# Level5
+url = 'https://apis.cbs.gov.il/series/catalog/level?id=4&subject=' + path1 + '&format=xml&download=false'
+# Send a GET request to the URL
+response = requests.get(url)
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the XML content
+    root = ET.fromstring(response.content)
+    # Extract the data you need from the XML and store it in a list of dictionaries
+    data = []
+    for level in root.findall(".//Level"):
+        # Extract relevant data from each item and store it in a dictionary
+        item_data = {
+            "path": ", ".join([str(int_element.text) for int_element in level.findall("path/int")]),
+            "name": level.find("name").text,
+        }
+        data.append(item_data)
+    # Create a DataFrame from the list of dictionaries
+    df5 = pd.DataFrame(data)
+    # Now you can work with the DataFrame
+    # For example, you can display the first few rows:
+    choice5 = st.selectbox('Level 5 - Select area of interest', df5['name'])
+    path5 = (df5['path'][df5['name'].tolist().index(choice5)])
+    st.write(path5)
