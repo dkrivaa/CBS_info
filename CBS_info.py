ba2546eb86118a914fb8229a59a05a0c61dd58f5
dkrivaa <dannykrivaa@hotmail.com>
2023-06-15 00:23:49 +0300

    App to see CBS data-series info

--- CBS_info.py
+++ CBS_info.py
@@ -80,8 +80,9 @@ if response.status_code == 200:
     len2 = len(path2)
     df3 = df3.loc[df3['path'].str[:len2] == path2]
     choice3 = st.selectbox('Level 3 - Select area of interest', df3['name'])
-    path3 = (df3['path'][df3['name'].tolist().index(choice3)])
-    st.write(path3)
+    st.write([df3['name'].tolist().index(choice3)])
+    # path3 = (df3['path'][df3['name'].tolist().index(choice3)])
+    # st.write(path3)
 
 # Level4
 url = 'https://apis.cbs.gov.il/series/catalog/level?id=4&subject=' + path1 + '&format=xml&download=false'
