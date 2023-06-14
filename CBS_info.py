b1a2aa19bffdffcd0a8f7a4b1109d5db4b192ad9
dkrivaa <dannykrivaa@hotmail.com>
2023-06-14 23:24:29 +0300

    App to see CBS data-series info

--- CBS_info.py
+++ CBS_info.py
@@ -78,7 +78,7 @@ if response.status_code == 200:
     df3 = pd.DataFrame(data)
     # Now you can work with the DataFrame
     # For example, you can display the first few rows:
-    # choice3 = st.selectbox('Level 3 - Select area of interest', df3['name'])
-    # path3 = (df3['path'][df3['name'].tolist().index(choice3)])
-    # st.write(path3[2])
+    choice3 = st.selectbox('Level 3 - Select area of interest', df3['name'])
+    path3 = (df3['path'][df3['name'].tolist().index(choice3)])
+    st.write(path3[2])
     st.dataframe(df3)
