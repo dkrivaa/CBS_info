337d9db6d86f82257e0a85f08f9c7453eae01b23
dkrivaa <dannykrivaa@hotmail.com>
2023-06-14 23:45:50 +0300

    App to see CBS data-series info

--- CBS_info.py
+++ CBS_info.py
@@ -79,7 +79,7 @@ if response.status_code == 200:
     # For example, you can display the first few rows:
     len2 = len(path2)
     choice3 = st.selectbox('Level 3 - Select area of interest',
-                           df3.loc[df3['name'][0:len2+1] == path2])
+                           df3['name'].loc[df3['path'][0:len2+1] == path2])
     # path3 = (df3['path'][df3['name'].tolist().index(choice3)])
     # st.write(path3)
 
