d9d715e047ad65e524ccb18ecec1a355ed32db03
dkrivaa <dannykrivaa@hotmail.com>
2023-06-15 16:05:06 +0300

    App to see CBS data-series info

--- CBS_info.py
+++ CBS_info.py
@@ -80,8 +80,10 @@ if response.status_code == 200:
     len2 = len(path2)
     dfi = df3.loc[df3['path'].str[:len2] == path2]
     choice3 = st.selectbox('Level 3 - Select area of interest', dfi['name'])
-    st.write(len(dfi['name'].tolist()))
-    path3 = (dfi['path'][dfi['name'].tolist().index(choice3)])
+    if len(dfi['name'].tolist()) > 1:
+        path3 = (dfi['path'][dfi['name'].tolist().index(choice3)])
+    else:
+        path3 = dfi['path'][0]
     st.write(path3)
 
 # Level4
