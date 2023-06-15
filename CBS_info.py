542930a0ee1ee5299367f559c6a9829371d7d85e
dkrivaa <dannykrivaa@hotmail.com>
2023-06-15 16:26:03 +0300

    App to see CBS data-series info

--- CBS_info.py
+++ CBS_info.py
@@ -79,13 +79,17 @@ if response.status_code == 200:
     # For example, you can display the first few rows:
     len2 = len(path2)
     dfi = df3.loc[df3['path'].str[:len2] == path2]
-    if len(dfi['name'].tolist()) > 1:
-        choice3 = st.selectbox('Level 3 - Select area of interest', dfi['name'])
-        st.write(dfi['path'][1])
-        # path3 = (dfi['path'][dfi['name'].tolist().index(choice3)])
-    else:
-        choice3 = st.selectbox('Level 3 - Select area of interest', dfi['name'])
+    if 0 in dfi.index:
         st.write(dfi['path'][0])
+    else:
+        st.write("Key 0 not found in the index.")
+    # if len(dfi['name'].tolist()) > 1:
+    #     choice3 = st.selectbox('Level 3 - Select area of interest', dfi['name'])
+    #     st.write(dfi['path'][1])
+    #     # path3 = (dfi['path'][dfi['name'].tolist().index(choice3)])
+    # else:
+    #     choice3 = st.selectbox('Level 3 - Select area of interest', dfi['name'])
+    #     st.write(dfi['path'][0])
         # path3 = dfi['path'][0]
     # st.write(path3)
 
